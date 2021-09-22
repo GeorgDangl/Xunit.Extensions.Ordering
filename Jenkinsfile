97ab09c8f4c36b09228cc6cd2014bcc5e243d71a
Georg Dangl <georg@dangl.me>
2021-09-22 11:18:12 +0200

    Configure build script

---
+++ Jenkinsfile
@@ -0,0 +1,76 @@
+pipeline {
+    options {
+        disableConcurrentBuilds()
+        timeout(time: 1, unit: 'HOURS')
+    }
+    agent {
+        node {
+            label 'master'
+        }
+    }
+    environment {
+        KeyVaultBaseUrl = credentials('AzureCiKeyVaultBaseUrl')
+        KeyVaultClientId = credentials('AzureCiKeyVaultClientId')
+        KeyVaultClientSecret = credentials('AzureCiKeyVaultClientSecret')
+    }
+    stages {
+        stage ('Test') {
+            steps {
+                powershell './build.ps1 Coverage -configuration Debug'
+            }
+            post {
+                always {
+                    recordIssues(
+                        tools: [
+                            msBuild(), 
+                            taskScanner(
+                                excludePattern: '**/*node_modules/**/*', 
+                                highTags: 'HACK, FIXME', 
+                                ignoreCase: true, 
+                                includePattern: '**/*.cs, **/*.g4, **/*.ts, **/*.js', 
+                                normalTags: 'TODO')
+                            ])
+                    xunit(
+                        testTimeMargin: '3000',
+                        thresholdMode: 1,
+                        thresholds: [
+                            failed(failureNewThreshold: '0', failureThreshold: '0', unstableNewThreshold: '0', unstableThreshold: '0'),
+                            skipped(failureNewThreshold: '0', failureThreshold: '0', unstableNewThreshold: '0', unstableThreshold: '0')
+                        ],
+                        tools: [
+                            xUnitDotNet(deleteOutputFiles: true, failIfNotNew: true, pattern: '**/*testresults*.xml', stopProcessingIfError: true)
+                        ])
+                    cobertura(
+                        coberturaReportFile: 'output/Cobertura.xml',
+                        failUnhealthy: false,
+                        failUnstable: false,
+                        maxNumberOfBuilds: 0,
+                        onlyStable: false,
+                        zoomCoverageChart: false)
+                    publishHTML([
+                        allowMissing: false,
+                        alwaysLinkToLastBuild: false,
+                        keepAll: false,
+                        reportDir: 'output/CoverageReport',
+                        reportFiles: 'index.htm',
+                        reportName: 'Coverage Report',
+                        reportTitles: ''])
+                }
+            }
+        }
+        stage ('Deploy') {
+            steps {
+                powershell './build.ps1 Push+PublishGitHubRelease'
+            }
+        }
+    }
+    post {
+        always {
+            step([$class: 'Mailer',
+                notifyEveryUnstableBuild: true,
+                recipients: "georg@dangl.me",
+                sendToIndividuals: true])
+            cleanWs()
+        }
+    }
+}
---
+++ build/Build.cs
@@ -0,0 +1,285 @@
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.AzureKeyVault.Attributes;
+using Nuke.Common.Tools.Coverlet;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.ReportGenerator;
+using Nuke.Common.Utilities.Collections;
+using Nuke.GitHub;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.IO.XmlTasks;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.ReportGenerator.ReportGeneratorTasks;
+using static Nuke.GitHub.GitHubTasks;
+
+[CheckBuildProjectConfigurations]
+[ShutdownDotNetAfterServerBuild]
+class Build : NukeBuild
+{
+    public static int Main() => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
+
+    [KeyVaultSettings(
+        BaseUrlParameterName = nameof(KeyVaultBaseUrl),
+        ClientIdParameterName = nameof(KeyVaultClientId),
+        ClientSecretParameterName = nameof(KeyVaultClientSecret))]
+    readonly KeyVaultSettings KeyVaultSettings;
+
+    [Parameter] string KeyVaultBaseUrl;
+    [Parameter] string KeyVaultClientId;
+    [Parameter] string KeyVaultClientSecret;
+
+    [KeyVaultSecret] string PublicMyGetSource;
+    [KeyVaultSecret] string PublicMyGetApiKey;
+    [KeyVaultSecret] string NuGetApiKey;
+    [KeyVaultSecret] string GitHubAuthenticationToken;
+
+    AbsolutePath OutputDirectory => RootDirectory / "output";
+
+    Target Clean => _ => _
+        .Executes(() =>
+        {
+            GlobDirectories(RootDirectory / "Xunit.Extensions.Ordering", "**/bin", "**/obj").ForEach(DeleteDirectory);
+            GlobDirectories(RootDirectory / "Xunit.Extensions.Ordering.Tests", "**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(OutputDirectory);
+        });
+
+    Target Restore => _ => _
+    .DependsOn(Clean)
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .EnableNoRestore());
+        });
+
+    Target Coverage => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            var testProjects = new[]
+            {
+                RootDirectory / "Xunit.Extensions.Ordering.Tests" / "Xunit.Extensions.Ordering.Tests.csproj"
+            };
+
+            var hasFailedTests = false;
+            try
+            {
+                DotNetTest(c => c
+                    .EnableNoBuild()
+                    .SetTestAdapterPath(".")
+                    .CombineWith(cc => testProjects
+                        .SelectMany(testProject =>
+                        {
+                            var projectDirectory = Path.GetDirectoryName(testProject);
+                            var projectName = Path.GetFileNameWithoutExtension(testProject);
+                            var targetFrameworks = GetTestFrameworksForProjectFile(testProject);
+                            return targetFrameworks.Select(targetFramework => cc
+                                // Coverage data is only collected for .NET Core or .NET 5 and newer
+                                .When(!targetFramework.StartsWith("net4"), ccc => ccc
+                                    .EnableCollectCoverage()
+                                    .SetCoverletOutputFormat(CoverletOutputFormat.cobertura)
+                                    .SetCoverletOutput($"{OutputDirectory / projectName}_coverage.xml")
+                                    .SetProcessArgumentConfigurator(a => a
+                                        .Add($"/p:Include=[Xunit.Extensions.Ordering*]*")
+                                        .Add($"/p:ExcludeByAttribute=\\\"Obsolete,GeneratedCodeAttribute,CompilerGeneratedAttribute\\\"")
+                                        // This is required for the .NET Framework tests, otherwise strong named assemblies would not be correctly
+                                        // found since Coverlet changes them in order to be able to generate a coverage result
+                                        .Add("-- RunConfiguration.DisableAppDomain=true")))
+                                .SetProjectFile(testProject)
+                                .SetFramework(targetFramework)
+                                .SetLoggers($"xunit;LogFilePath={OutputDirectory / projectName}_testresults-{targetFramework}.xml"));
+                        })),
+                            degreeOfParallelism: Environment.ProcessorCount,
+                            completeOnFailure: true);
+            }
+            catch
+            {
+                hasFailedTests = true;
+            }
+
+            PrependFrameworkToTestresults();
+
+            // This is the report that's pretty and visualized in Jenkins
+            ReportGenerator(c => c
+                .SetFramework("netcoreapp3.0")
+                .SetReports(OutputDirectory / "*_coverage*.xml")
+                .SetTargetDirectory(OutputDirectory / "CoverageReport"));
+
+            // Merge coverage reports, otherwise they might not be completely
+            // picked up by Jenkins
+            ReportGenerator(c => c
+                .SetFramework("netcoreapp3.0")
+                .SetReports(OutputDirectory / "*_coverage*.xml")
+                .SetTargetDirectory(OutputDirectory)
+                .SetReportTypes(ReportTypes.Cobertura));
+
+            if (hasFailedTests)
+            {
+                ControlFlow.Fail("Some tests have failed");
+            }
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetPack(x => x
+                .SetProcessArgumentConfigurator(a => a.Add("/nodeReuse:false"))
+                .SetConfiguration(Configuration)
+                .SetDescription("Dangl.Xunit.Extensions.Ordering - www.dangl-it.com")
+                .SetTitle("Dangl.Xunit.Extensions.Ordering - www.dangl-it.com")
+                .EnableNoBuild()
+                .SetOutputDirectory(OutputDirectory)
+                .SetVersion(GitVersion.NuGetVersion));
+        });
+
+    Target Push => _ => _
+        .DependsOn(Pack)
+        .Requires(() => PublicMyGetSource)
+        .Requires(() => PublicMyGetApiKey)
+        .Requires(() => NuGetApiKey)
+        .Requires(() => Configuration == Configuration.Release)
+        .Executes(() =>
+        {
+            GlobFiles(OutputDirectory, "*.nupkg").NotEmpty()
+                .Where(x => !x.EndsWith("symbols.nupkg"))
+                .ForEach(x =>
+                {
+                    DotNetNuGetPush(s => s
+                        .SetTargetPath(x)
+                        .SetSource(PublicMyGetSource)
+                        .SetApiKey(PublicMyGetApiKey));
+
+                    if (GitVersion.BranchName.Equals("master") || GitVersion.BranchName.Equals("origin/master"))
+                    {
+                        // Stable releases are published to NuGet
+                        DotNetNuGetPush(s => s
+                            .SetTargetPath(x)
+                            .SetSource("https://api.nuget.org/v3/index.json")
+                            .SetApiKey(NuGetApiKey));
+                    }
+                });
+        });
+
+    Target PublishGitHubRelease => _ => _
+        .DependsOn(Pack)
+        .Requires(() => GitHubAuthenticationToken)
+        .OnlyWhenDynamic(() => GitVersion.BranchName.Equals("master") || GitVersion.BranchName.Equals("origin/master"))
+        .Executes(async () =>
+        {
+            var releaseTag = $"v{GitVersion.MajorMinorPatch}";
+
+            var repositoryInfo = GetGitHubRepositoryInfo(GitRepository);
+            var nuGetPackages = GlobFiles(OutputDirectory, "*.nupkg").NotEmpty().ToArray();
+
+            await PublishRelease(x => x
+                    .SetArtifactPaths(nuGetPackages)
+                    .SetCommitSha(GitVersion.Sha)
+                    .SetRepositoryName(repositoryInfo.repositoryName)
+                    .SetRepositoryOwner(repositoryInfo.gitHubOwner)
+                    .SetTag(releaseTag)
+                    .SetToken(GitHubAuthenticationToken));
+        });
+
+    private IEnumerable<string> GetTestFrameworksForProjectFile(string projectFile)
+    {
+        var targetFrameworks = XmlPeek(projectFile, "//Project/PropertyGroup//TargetFrameworks")
+            .Concat(XmlPeek(projectFile, "//Project/PropertyGroup//TargetFramework"))
+            .Distinct()
+            .SelectMany(f => f.Split(';'))
+            .Distinct();
+        return targetFrameworks;
+    }
+
+    private void PrependFrameworkToTestresults()
+    {
+        var testResults = GlobFiles(OutputDirectory, "*testresults*.xml").ToList();
+        foreach (var testResultFile in testResults)
+        {
+            var frameworkName = GetFrameworkNameFromFilename(testResultFile);
+            var xDoc = XDocument.Load(testResultFile);
+
+            foreach (var testType in ((IEnumerable)xDoc.XPathEvaluate("//test/@type")).OfType<XAttribute>())
+            {
+                testType.Value = frameworkName + "+" + testType.Value;
+            }
+
+            foreach (var testName in ((IEnumerable)xDoc.XPathEvaluate("//test/@name")).OfType<XAttribute>())
+            {
+                testName.Value = frameworkName + "+" + testName.Value;
+            }
+
+            xDoc.Save(testResultFile);
+        }
+
+        // Merge all the results to a single file
+        // The "run-time" attributes of the single assemblies is ensured to be unique for each single assembly by this test,
+        // since in Jenkins, the format is internally converted to JUnit. Aterwards, results with the same timestamps are
+        // ignored. See here for how the code is translated to JUnit format by the Jenkins plugin:
+        // https://github.com/jenkinsci/xunit-plugin/blob/d970c50a0501f59b303cffbfb9230ba977ce2d5a/src/main/resources/org/jenkinsci/plugins/xunit/types/xunitdotnet-2.0-to-junit.xsl#L75-L79
+        var firstXdoc = XDocument.Load(testResults[0]);
+        var runtime = DateTime.Now;
+        var firstAssemblyNodes = firstXdoc.Root.Elements().Where(e => e.Name.LocalName == "assembly");
+        foreach (var assemblyNode in firstAssemblyNodes)
+        {
+            assemblyNode.SetAttributeValue("run-time", $"{runtime:HH:mm:ss}");
+            runtime = runtime.AddSeconds(1);
+        }
+        for (var i = 1; i < testResults.Count; i++)
+        {
+            var xDoc = XDocument.Load(testResults[i]);
+            var assemblyNodes = xDoc.Root.Elements().Where(e => e.Name.LocalName == "assembly");
+            foreach (var assemblyNode in assemblyNodes)
+            {
+                assemblyNode.SetAttributeValue("run-time", $"{runtime:HH:mm:ss}");
+                runtime = runtime.AddSeconds(1);
+            }
+            firstXdoc.Root.Add(assemblyNodes);
+        }
+
+        firstXdoc.Save(OutputDirectory / "testresults.xml");
+        testResults.ForEach(DeleteFile);
+    }
+
+    private string GetFrameworkNameFromFilename(string filename)
+    {
+        var name = Path.GetFileName(filename);
+        name = name.Substring(0, name.Length - ".xml".Length);
+        var startIndex = name.LastIndexOf('-');
+        name = name.Substring(startIndex + 1);
+        return name;
+    }
+}
