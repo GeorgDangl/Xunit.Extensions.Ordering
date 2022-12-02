f4af452edd553fe566ae7e7aa6613ee1c34081f7
Georg Dangl <georg@dangl.me>
2022-12-02 16:41:51 +0100

    Fix typo

--- Xunit.Extensions.Ordering/IAssemblyFixture.cs
+++ Xunit.Extensions.Ordering/IAssemblyFixture.cs
@@ -1,7 +1,7 @@
 ﻿namespace Xunit.Extensions.Ordering
 {
 	/// <summary>
-	/// Interface do decorate you test classes to register assembly level fixture.
+	/// Interface to decorate you test classes to register assembly level fixture.
 	/// </summary>
 	/// <typeparam name="TFixture">Assembly fixture to register</typeparam>
 	public interface IAssemblyFixture<TFixture> 
